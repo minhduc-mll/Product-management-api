52058cbd14b372081c9b2fd3896700076435ee40
minhduc <minhduc.mll@gmail.com>
2023-06-13 00:43:58 +0700

    Change cors options

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        credentials: true,
-    })
-);
+const corsOptions = {
+    origin: ["*", "http://localhost:3000"],
+    credentials: true, //access-control-allow-credentials:true
+};
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: false }));

ede92272963685b74372f27c0bfdf0a07a9a3c52
minhduc <minhduc.mll@gmail.com>
2023-06-16 14:23:34 +0700

    Fix post form null object

--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const TaskController = require("../controllers/TaskController");
+const multer = require("../middlewares/Multer");
 
 router.get("/userTasks", TaskController.getUserTasks);
 
@@ -8,7 +9,7 @@ router.get("/", TaskController.getAllTask);
 router.get("/:id", TaskController.getTask);
 router.get("/user/:userId", TaskController.getTaskByUserId);
 
-router.post("/", TaskController.createTask);
+router.post("/", multer.single("image"), TaskController.createTask);
 
 router.put("/:id", TaskController.updateTask);
 router.patch("/:id", TaskController.updateTask);
